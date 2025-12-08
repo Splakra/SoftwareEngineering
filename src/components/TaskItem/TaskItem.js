18ee0f43558cd25a28c3599cfa195906afd87e49
lucym <lucy.maege@haw-hamburg.de>
2025-12-08 16:43:46 +0100

    feat: redesign TaskItem component with new layout and gradient animation

--- src/components/TaskItem/TaskItem.js
+++ src/components/TaskItem/TaskItem.js
@@ -3,33 +3,36 @@ import PersonIcon from "../../assets/person-round.svg";
 import Checkmark from "../../assets/checkmark.svg";
 import "./TaskItem.css";
 
-export default function TaskItem ({name, medication}) {
+export default function TaskItem({name, medication, time}) {
     const [checked, setChecked] = useState(false);
     const [checkedDate, setCheckedDate] = useState(null);
     const handleChecked = () => {
-        const date = new Date().toLocaleTimeString();
+        const date = new Date().toLocaleTimeString(navigator.language, {hour: '2-digit', minute: '2-digit'});
         setChecked(true);
         setCheckedDate(date);
     }
     const status = checked ? `Eingenommen: ${checkedDate}` : "Ausstehend";
     return (
-        <div className={"task-item"}>
-            <div className={"task-item__infos"}>
-                <div className={"task-item__profile"}>
-                    <div className={"task-item__icon"}>
-                        <img alt="" className={"task-item__person"} src={PersonIcon}/>
-                    </div>
-                    {name}
-                </div>
-                <div className={"task-item__medication"}>
-                    {medication}
-                </div>
-                <div className={"task-item__status"}>
-                    {status}
-                </div>
+        <div className={`task-item ${checked ? "task-item--checked" : ""}`}>
+            <div className={"task-item__time"}>
+                {time}
             </div>
             <button onClick={handleChecked} className={"task-item__button"}>
-                <img alt="" className={"task-item__checkmark"} src={Checkmark}/>
+                <div className={"task-item__infos"}>
+                    <div className={"task-item__profile"}>
+                        <img alt="" className={"task-item__person"} src={PersonIcon}/>
+                        {name}
+                    </div>
+                    <div className={"task-item__medication"}>
+                        {medication}
+                    </div>
+                    <div className={"task-item__status"}>
+                        {status}
+                    </div>
+                </div>
+                <div className="task-item__checkmark-wrapper">
+                    <img alt="" className="task-item__checkmark" src={Checkmark}/>
+                </div>
             </button>
         </div>
     )
