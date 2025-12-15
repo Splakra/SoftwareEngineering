b144809b0302383b5a5216cd06cf32d0cf1a7a3e
lucym <lucy.maege@haw-hamburg.de>
2025-12-15 17:41:37 +0100

    fix(Dexie): correct naming and add comments for schema naming

--- src/database/DexieDatabase.js
+++ src/database/DexieDatabase.js
@@ -3,8 +3,10 @@ import Dexie from "dexie";
 var db = new Dexie('MyDatabase');
 db.version(1).stores({
     medication: '++id, name, type, amount, reminderBuyNew, expiration, reminderExpiration',
-    intakeMeds: '++id, medication, patient, rythm, startDate, endDate, time, dose, weekdays, interval',
+    intakeMeds: '++id, medication, patient, rhythm, startDate, endDate, time, dose, weekdays, interval',
+    // reminders: ..., medicationId, patientId, ...
     profiles: '++id, name'
+    // profileS/medicationS/reminderS - either plural or singular, not mixed
 });
 
 
