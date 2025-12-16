eb35ddf980752817d1d10007f431848ddf02f0ee
fngrlknd <103189467+fngrlknd@users.noreply.github.com>
2025-12-16 15:31:25 +0100

    profiles anzeige

--- src/pages/profiles/profiles.jsx
+++ src/pages/profiles/profiles.jsx
@@ -2,6 +2,8 @@ import PersonIcon from "../../assets/person-round.svg";
 import "./Profiles.css"
 import NewProfile from "./newProfile";
 import {useNavigate} from "react-router";
+import db from "../../database/DexieDatabase";
+import {useEffect, useState} from "react";
 
 export default function Profile({name}) {
     const navigate = useNavigate();
@@ -10,15 +12,35 @@ export default function Profile({name}) {
         navigate("/profile/add");
     }
 
+    const [patients, setPatients] = useState([])
+    useEffect(() => {
+        async function loadPatients() {
+            const loadedPatients = await db.profiles.toArray();
+            setPatients(loadedPatients);
+        }
+
+        loadPatients();
+    }, [])
+
+
     return (
         <div className={"profile-item"}>
-            <div className = {"profile__name"}>
+            <div className={"profile__name"}>
                 <div className={"task-item__icon"}>
                     <img alt="" className={"task-item__person"} src={PersonIcon}/>
                 </div>
-                {name}
+                {
+                    patients.map(profile => {
+                        return <div>
+                            {
+                                profile.name
+                            }
+
+                        </div>
+                    })
+                }
             </div>
-            <button onClick={handleClick}> Profil hinzufügen </button>
+            <button onClick={handleClick}> Profil hinzufügen</button>
         </div>
     )
 }
