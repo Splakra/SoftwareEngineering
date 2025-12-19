e5d1b4a661433716ecc8621b683d4295a3a315b5
lucym <lucy.maege@haw-hamburg.de>
2025-12-19 21:14:39 +0100

    refactor: small changes in padding styles

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,9 @@ import {
 } from "@react-router/dev/routes";
 
 export default [
-    route("/", "./pages/medicinePage/MedsTester.jsx"),
+    route("/", "./pages/dashboard/dashboard.jsx"),
+    // route("/", "./pages/medicinePage/MedsTester.jsx"),
+
     // * matches all URLs, the ? makes it optional so it will match / as well
     route("*?", "catchall.jsx"),
 ];
