67e2180f1ecc3fef160c5cf2d492a186c6e30987
fngrlknd <103189467+fngrlknd@users.noreply.github.com>
2026-02-03 19:38:20 +0100

    zurück button funktioniert auf android

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,18 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { GlobalProvider } from "./pages/globalContext";
 
+import { App as CapacitorApp } from '@capacitor/app';
+
+CapacitorApp.addListener('backButton', ({ canGoBack }) => {
+    if (canGoBack) {
+        window.history.back();
+    } else {
+        // Optional: App nicht schließen 
+        // z.B. Toast anzeigen oder minimieren 
+        // App.exitApp(); // nur wenn du wirklich schließen willst 
+    }
+});
+
 ReactDOM.createRoot(document.getElementById("root")).render(
     <BrowserRouter>
         <GlobalProvider>
