d2204329aa637878799bff75b091b24159df22b2
fngrlknd <103189467+fngrlknd@users.noreply.github.com>
2026-02-03 20:13:10 +0100

    navbar problem behoben

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { GlobalProvider } from "./pages/globalContext";
-
 import { App as CapacitorApp } from '@capacitor/app';
+import { SafeArea } from '@capacitor-community/safe-area';
 
 CapacitorApp.addListener('backButton', ({ canGoBack }) => {
     if (canGoBack) {
@@ -12,8 +12,14 @@ CapacitorApp.addListener('backButton', ({ canGoBack }) => {
     } else {
         // Optional: App nicht schließen 
         // z.B. Toast anzeigen oder minimieren 
-        // App.exitApp(); // nur wenn du wirklich schließen willst 
+        CapacitorApp.exitApp(); // nur wenn du wirklich schließen willst 
     }
+});
+SafeArea.getSafeAreaInsets().then(({ insets }) => {
+    document.documentElement.style.setProperty(
+        '--safe-area-inset-bottom',
+        `${insets.bottom}px`
+    );
 });
 
 ReactDOM.createRoot(document.getElementById("root")).render(
