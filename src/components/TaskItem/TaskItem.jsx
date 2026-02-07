473df8b506239b6c6f4103271c0a7f138e1c3a60
Anna <103189467+fngrlknd@users.noreply.github.com>
2026-02-07 01:13:38 +0100

    runden, icons und datums bug gefixt (#5)
    * runden in richtig
    
    * ich bin magie digga
    
    * ich bin so richtig magie digga

--- src/components/TaskItem/TaskItem.jsx
+++ src/components/TaskItem/TaskItem.jsx
@@ -1,4 +1,4 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import PawIcon from "../../components/Icons/PawIcon.jsx";
 import Checkmark from "../../assets/checkmark.svg";
 import TrashIcon from "../../assets/trash.svg";
@@ -6,9 +6,9 @@ import ReverseIcon from "../../assets/reverse.svg";
 import "./TaskItem.css";
 import ToggleMenu from "../ToggleMenu/ToggleMenu";
 import db from "../../database/DexieDatabase";
-import {getDoseUnit} from "../../utils/therapyFormat";
+import { getDoseUnit } from "../../utils/therapyFormat";
 
-export default function TaskItem({patient, medication, time, date, dose, id, done}) {
+export default function TaskItem({ patient, medication, time, date, dose, id, done }) {
     const doseText = `${dose} ${getDoseUnit(medication.type)}`;
     const isDone = done.find((done) => done.date === date && done.time === time);
     const [checkedTime, setCheckedTime] = useState(isDone?.intakeTime || null);
@@ -32,7 +32,7 @@ export default function TaskItem({patient, medication, time, date, dose, id, don
         if (checked) return;
 
         const now = new Date();
-        const timeString = now.toLocaleTimeString(navigator.language, {hour: "2-digit", minute: "2-digit"});
+        const timeString = now.toLocaleTimeString(navigator.language, { hour: "2-digit", minute: "2-digit" });
 
         const doseTaken = Number(convertDoseToAmount(dose, medication.type));
         const updatedMedication = await db.medications.get(medication.id);
@@ -47,7 +47,8 @@ export default function TaskItem({patient, medication, time, date, dose, id, don
         });
 
         // reduce medication inventory
-        await db.medications.update(medication.id, {amount: Number(updatedMedication.amount) - doseTaken});
+        const newAmount = Number.parseFloat((Number(updatedMedication.amount) - doseTaken).toFixed(2))
+        await db.medications.update(medication.id, { amount: newAmount });
 
         setChecked(true);
         setCheckedTime(timeString);
@@ -106,10 +107,10 @@ export default function TaskItem({patient, medication, time, date, dose, id, don
             </div>
             <div onClick={handleChecked} className={"task-item__button"}>
                 <ToggleMenu className="task-item-toggle"
-                            {...{items}}/>
+                    {...{ items }} />
                 <div className={"task-item__infos"}>
                     <div className={"task-item__profile"}>
-                        <PawIcon className="task-item__icon"/>
+                        <PawIcon className="task-item__icon" />
                         <div className={"task-item__profile-name with-ellipsis"}>
                             {patient.name}
                         </div>
@@ -125,7 +126,7 @@ export default function TaskItem({patient, medication, time, date, dose, id, don
                     </div>
                 </div>
                 <div className="task-item__checkmark-wrapper">
-                    <img alt="" className="task-item__checkmark" src={Checkmark}/>
+                    <img alt="" className="task-item__checkmark" src={Checkmark} />
                 </div>
             </div>
         </div>
